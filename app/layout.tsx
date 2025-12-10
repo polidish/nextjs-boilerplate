14009ee3fa83518f174d66708169b333c0f3fab7
polidish <jeni@polidish.com>
2025-12-10 02:14:27 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
 return (
 <html lang="en">
-<body className={playfair.className}>{children}</body>
+<body>{children}</body>
 </html>
 );
 }
