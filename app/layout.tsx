145bc7799d8c4f0389a42383b1246119bf658d48
polidish <jeni@polidish.com>
2025-12-10 02:09:57 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import type { ReactNode } from "react";
-import { playfair } from "./fonts";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 return (
