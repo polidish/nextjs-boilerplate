293583df0d1edc83e0aec9abc8c209b01749499f
polidish <jeni@polidish.com>
2026-01-25 22:08:58 -0600

    Create page.tsx

---
+++ app/assurance/page.tsx
@@ -0,0 +1,14 @@
+export default function AssurancePage() {
+return (
+<main
+style={{
+maxWidth: 720,
+margin: '0 auto',
+padding: '48px 24px',
+fontFamily: 'serif',
+}}
+>
+<h1>Data Assurance</h1>
+</main>
+);
+}
